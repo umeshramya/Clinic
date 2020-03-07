b8696bb742dedad7871d099188e8a321ae546ff5
umeshramya <umeshbilagi@gmail.com>
2020-03-07 20:59:32 +0530

    do while instead while

--- login.cpp
+++ login.cpp
@@ -25,41 +25,49 @@ void Login::on_pushButton_Login_clicked()
     User user;
     Role role;
 
-    DataConnection::getInstance()->openConnection();
+
 
     try {
+        DataConnection::getInstance()->openConnection();//opem connection
+        QString userName = ui->lineEdit_userName->text();//set username
+        QString password = ui->lineEdit_password->text();//set password
 
-        QString userName = ui->lineEdit_userName->text();
-        QString password = ui->lineEdit_password->text();
-        QString sql =  SQL::LoginUser();
+        QString sql =  SQL::LoginUser();//sql string
 
-        QSqlQuery query;
-        query.prepare(sql);
-        query.bindValue(":username", userName);
-        query.bindValue(":password", password);
-        query.exec();
-        while(query.next()){
-            //id, username, name, email, mobile, address, is_doctor, is_active, role"
-            QMessageBox::information(this, "", query.value(1).toString());
-            role.setCurRole(query.value(8).toString());
+        QSqlQuery query;//sql query
+        query.prepare(sql);//prepare
+        query.bindValue(":username", userName);//bind username
+        query.bindValue(":password", password);//bind password
+        query.exec();//exec
 
-            user.setId(query.value(0).toInt());
-            user.setUserName(query.value(1).toString());
-            user.setName(query.value(2).toString());
-            user.setEmail(query.value(3).toString());
-            user.setMobile(query.value(4).toString());
-            user.setAddress(query.value(5).toString());
-            user.setIsDoctor(query.value(6).toBool());
-            user.setIsActive(query.value(7).toBool());
-            user.setRole(query.value(8).toString());
-
+        if(!query.first()){
+            throw QString("Invalid username and password");
         }
 
-        DataConnection::getInstance()->closeConnection();
-        Login::close();
+
+        do{                 //loop of dataset
+            //id, username, name, email, mobile, address, is_doctor, is_active, role"
+
+            role.setCurRole(query.value(8).toString());//set role and throw error if
+
+            user.setId(query.value(0).toInt());//id
+            user.setUserName(query.value(1).toString());//userbname
+            user.setName(query.value(2).toString());//name
+            user.setEmail(query.value(3).toString());//email
+            user.setMobile(query.value(4).toString());//mobile
+            user.setAddress(query.value(5).toString());//address
+            user.setIsDoctor(query.value(6).toBool());//is_doctor
+            user.setIsActive(query.value(7).toBool());//is_active
+            user.setRole(query.value(8).toString());//role
+
+        }while(query.next());
+
+        DataConnection::getInstance()->closeConnection();//close connection
+        Login::close();//close login ui
 
     } catch (QString error) {
-        QMessageBox::critical(this, "Invalid Login", error);
+        DataConnection::getInstance()->closeConnection();//close connection
+        QMessageBox::critical(this, "Invalid Login", error);//error message box
     }
 
 }
