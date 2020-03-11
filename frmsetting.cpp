16da5835b6d5298c85011ecf40654005eb6d3e24
umeshramya <umeshbilagi@gmail.com>
2020-03-11 15:51:26 +0530

    fired setting from mainwindow constructor

--- frmsetting.cpp
+++ frmsetting.cpp
@@ -12,35 +12,6 @@ FrmSetting::FrmSetting(QWidget *parent) :
     ui->setupUi(this);
     Settings* setting = Settings::getInstance();
 
-
-    QFile settingFile("settingFile.txt");//create file inside direct
-
-    if(!settingFile.open(QFile::ReadOnly| QFile::Text)){//check for if exist
-        settingFile.open(QFile::WriteOnly | QFile::Text);//if not exist create new file with defualt6 parametrs
-        QTextStream out(&settingFile);//set write stream for file
-        QString fileData =QString(setting->getDataBaseConnectionString() + "\n");
-        fileData.append(setting->getTemplateFolderPath() + "\n");
-        fileData.append(QString::number(setting->getLetterPadHeaderHeight()) + "\n");
-        fileData.append(setting->getLetterpadType() + "\n");
-        out << fileData;
-        settingFile.flush();//flush after writing into file
-        settingFile.close();//close file
-
-    }else{//if file exist read the content
-        QTextStream in(&settingFile);//create reading stream
-        setting->setDataBaseConnectionString(in.readLine());//read line by line
-        setting->setTemplateFolderPath(in.readLine());
-        setting->setLetterPadHeaderHeight(in.readLine().toInt());
-        setting->setLetterpadType(in.readLine());
-        settingFile.close();
-
-    }
-
-
-
-
-
-
     ui->lineEdi_databaseConnection->setText(setting->getDataBaseConnectionString());
     ui->lineEdit_TemplateFolderPath->setText(setting->getTemplateFolderPath());
     ui->spinBox_LetterpadHeaderLines->setValue(setting->getLetterPadHeaderHeight());
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "Classes/dataconnection.h"
+#include "Classes/settings.h"
+
+#include "QFile"
+#include "QDebug"
+#include "QTextStream"
+
 #include "login.h"
 #include "newuser.h"
 #include "frmsetting.h"
@@ -10,6 +16,33 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    Settings* setting = Settings::getInstance();//set the settings
+
+    QFile settingFile("settingFile.txt");//create file inside direct
+
+    if(!settingFile.open(QFile::ReadOnly| QFile::Text)){//check for if exist
+        settingFile.open(QFile::WriteOnly | QFile::Text);//if not exist create new file with defualt6 parametrs
+        QTextStream out(&settingFile);//set write stream for file
+        QString fileData =QString(setting->getDataBaseConnectionString() + "\n");
+        fileData.append(setting->getTemplateFolderPath() + "\n");
+        fileData.append(QString::number(setting->getLetterPadHeaderHeight()) + "\n");
+        fileData.append(setting->getLetterpadType() + "\n");
+        out << fileData;
+        settingFile.flush();//flush after writing into file
+        settingFile.close();//close file
+
+    }else{//if file exist read the content
+        QTextStream in(&settingFile);//create reading stream
+        setting->setDataBaseConnectionString(in.readLine());//read line by line
+        setting->setTemplateFolderPath(in.readLine());
+        setting->setLetterPadHeaderHeight(in.readLine().toInt());
+        setting->setLetterpadType(in.readLine());
+        settingFile.close();
+
+    }
+
+
 }
 
 MainWindow::~MainWindow()
